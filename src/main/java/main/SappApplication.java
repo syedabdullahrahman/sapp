a9b58432c4539a94c9b1a6bb4c01f198a5ba45e4
Krzysiek <viruss.snk@gmail.com>
2017-01-03 10:07:26 +0100

    Initial commit - Spring Boot: web + thymeleaf

---
+++ src/main/java/main/SappApplication.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SappApplication.class, args);
+	}
+}
